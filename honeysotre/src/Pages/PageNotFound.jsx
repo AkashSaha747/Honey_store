b42b4a60232eb2392105ccd3736e7b54c9987370
AkashSaha747 <akashkaliyaganj5@gmail.com>
2023-06-01 14:35:24 +0530

    footer, langing page, page not found page is done

--- honeysotre/src/Pages/PageNotFound.jsx
+++ honeysotre/src/Pages/PageNotFound.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { Box, Button, Center, Flex, Text,Image } from '@chakra-ui/react'
+import { useNavigate } from 'react-router-dom'
 const PageNotFound = () => {
+  let navigate=useNavigate();
   return (
-    <div>
-    <h1> page not found</h1> 
-    </div>
+    <Flex  w={"40%"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} m={" 40px auto"} gap={"20px"}>
+    <Image src='https://cdn.discordapp.com/attachments/1100180350879154326/1113752777345208350/image.png'></Image> 
+    <Text fontWeight={"bold"}>OOPS... THE REQUESTED PAGE DOES NOT EXIST</Text>
+    <Button colorScheme='teal' variant='outline' borderRadius={"20px"} w={"30%"} onClick={()=>{
+navigate("/");
+    }}>GO BACK</Button>
+    </Flex>
   )
 }
 
