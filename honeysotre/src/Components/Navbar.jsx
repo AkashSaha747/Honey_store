ebffad8d16e3be9e69d98e6c2787da5c221946a1
KaunainRizvi <krizvibusiness.com>
2023-06-03 15:34:47 +0530

    Login_Signup

--- honeysotre/src/Components/Navbar.jsx
+++ honeysotre/src/Components/Navbar.jsx
@@ -101,3 +101,141 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+// import { Box, Center, Flex, Heading,Badge, Spacer,Text,InputGroup,Input,InputRightElement,Button } from '@chakra-ui/react'
+// import React, { useState }  from 'react'
+// import { AiOutlineSearch } from 'react-icons/ai';
+// import { MdOutlineAccountCircle } from 'react-icons/md';
+// import { BsCart2 } from 'react-icons/bs';
+// import { useAuth0 } from "@auth0/auth0-react";
+
+// import '../Styles/ButtonStyle.css';
+
+
+
+// const Navbar = () => {
+  
+//     const { loginWithRedirect, isAuthenticated , logout, user } = useAuth0();
+//   const [showSearchBar, setShowSearchBar] =  useState(false);
+//   const [searchQuery, setSearchQuery] = useState('');
+//   const handleButtonClick = () => {
+//     setShowSearchBar(!showSearchBar);
+//   };
+//   const handleSearchQueryChange = (event) => {
+//     setSearchQuery(event.target.value);
+//   };
+
+//   const handleSearch = () => {
+//     // console.log('Search query:', searchQuery);
+//   };
+
+
+//   const cartItemCount = 5;
+
+//   return (
+//     <Box border={"2px solid rgb(42,40,42)"} style={{width:"100%"}} h={"12vh"}>
+//       <Text style={{background:"#185E49",color:"white",width:"100%",textAlign:"center",}}> Free shipping on orders over 500 rupees </Text>
+   
+   
+   
+//     <Flex style={{width:"80%",margin:"0px auto",padding:"0px",justifyContent:"space-between",alignItems:"center",}}>
+
+//       <Box borderLeft={"2px solid black"} borderRight={"2px solid black"} paddingLeft={"10"}  paddingRight={"10"}  h={"50px"}>
+//         <Heading>LET IT BEE.CO</Heading>
+//       </Box>
+
+//       <Box>
+
+//       {showSearchBar ? (
+//         <InputGroup >
+//           <Input
+//             placeholder="Search..."
+//             value={searchQuery}
+//             onChange={handleSearchQueryChange}
+//           />
+//           <InputRightElement width="4.5rem">
+//             <Button  size="sm" onClick={handleSearch}>
+//               Search
+//             </Button>
+//           </InputRightElement>
+//         </InputGroup>
+//       ):<Spacer/>}
+//       </Box>
+
+
+//       <Flex alignItems={"center"} justifyContent={"center"} gap={"40px"}>
+
+//         <Box onClick={handleButtonClick} pt={"10px"} pr={"30px"} pl={" 30px "} borderLeft={"2px solid black"} borderRight={"2px solid black"} h={"50px"} ><AiOutlineSearch  style={{fontSize:"30px"}}/></Box>
+
+//         <Box  h={"50px"} pt={"10px"}><MdOutlineAccountCircle  style={{fontSize:"30px",display:"flex",margin:"auto"}}/></Box>
+
+//         <Box borderLeft={"2px solid black"}  h={"50px"} borderRight={"2px solid black"} p={"0 30px "}>
+//       <Box position="relative" display="inline-block" >
+//         <Box
+//           color="white"
+//           borderRadius="full"
+//           boxSize="30px"
+//           textAlign="center"
+//           fontSize="sm"
+//           fontWeight="bold"
+//           position="absolute"
+//           top="8px"
+//           right="-7px"
+//           zIndex="docked"
+//         >
+
+          
+
+//           <Badge borderRadius={"full"} background={"#F05A1F"}>{cartItemCount}</Badge>
+//         </Box>
+
+        
+//         <Box
+//           w="50px"
+//           h="50px"
+//           display="flex"
+//           alignItems="center"
+//           justifyContent="center"
+//           cursor="pointer"
+//         >
+//           {/* Your cart icon here */
+//           <BsCart2 style={{fontSize:"30px"}}/>
+//           }
+//         </Box>
+        
+//       </Box>
+//     </Box>
+
+  
+//     {
+//   isAuthenticated && 
+//   <li>
+//     <p>{"Hello, " + user.nickname}</p>
+//   </li>
+// }
+// <li>
+//   {
+//     isAuthenticated ? (
+//       <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })} style={{ backgroundColor: '#498070', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
+//         Log Out
+//       </button>
+//     ) : (
+//       <button onClick={() => loginWithRedirect()} style={{ backgroundColor: '#498070', color: '#fff', padding: '10px 20px', border: 'none', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)' }}>
+//         Log In
+//       </button>
+//     )
+//   }
+// </li>
+
+    
+          
+//       </Flex>
+//     </Flex>
+//     </Box>
+//   )
+// }
+
+// export default Navbar
+
+// Run in terminal : npm install @auth0/auth0-react
